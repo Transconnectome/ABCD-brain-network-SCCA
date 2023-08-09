699597184e1a56d6384cdaea04d3872709103eda
jungwoooseo <129498794+jungwoooseo@users.noreply.github.com>
2023-08-09 15:01:33 +0900

    Update scca_pipeline_example.sh

--- scca_pipeline_example.sh
+++ scca_pipeline_example.sh
@@ -17,6 +17,6 @@ python3 SCCA_hyper_param.py  --domain $1 --dataset $2 --exp_tag $3 \
 & (python3 SCCA_bootstrap.py --domain $1 --dataset $2 --exp_tag $3 --n_boot 5000 --n_start 1 \
 && python3 SCCA_bootstrap_result_summary.py --domain $1 --dataset $2 --exp_tag $3 --n_boot 5000)) \
 && (python3 non_BNM_vis.py --domain $1 --exp_tag $3) \
-&& (python3 bootstrap_vis_global_rich_club_BNM.py --domain $1 --exp_tag $3) \
+&& (python3 bootstrap_vis_global_BNM.py --domain $1 --exp_tag $3) \
 && (python3 SCCA_permutation_test.py --domain $1 --dataset testset --exp_tag $3 --n_perm 5000) \
 && (Rscript /scratch/connectome/seojw/"BNM analysis"/code/R_code/brain_visualize.R $1 $3)
